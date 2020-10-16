070ba4974f27f9abf93a47c17be99eb21c66ff3f
Matej Bogdanoski <matej.bogdanoski@students.finki.ukim.mk>
2020-10-16 23:09:40 +0200

    rearrange service

--- src/main/kotlin/mk/ukim/finki/reactive_survey_app/service/SurveyService.kt
+++ src/main/kotlin/mk/ukim/finki/reactive_survey_app/service/SurveyService.kt
@@ -1,20 +1,8 @@
 package mk.ukim.finki.reactive_survey_app.service
 
-import mk.ukim.finki.reactive_survey_app.domain.enum.QuestionType
-import mk.ukim.finki.reactive_survey_app.repository.SurveyRepository
 import mk.ukim.finki.reactive_survey_app.responses.SelectOptionResponse
-import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 
-@Service
-class SurveyService(
-        private val repository: SurveyRepository
-) {
-
-    fun findAllQuestionTypesAsSelectOptions(): Flux<SelectOptionResponse> = Flux.fromArray(QuestionType.values())
-            .map {
-                with(it) {
-                    SelectOptionResponse(id = ordinal.toLong(), label = name)
-                }
-            }
+interface SurveyService {
+    fun findAllQuestionTypesAsSelectOptions(): Flux<SelectOptionResponse>
 }
