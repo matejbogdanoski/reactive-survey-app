814db80f66b1f0b49e8c00fa1abf28f439e43dc8
Matej Bogdanoski <matej.bogdanoski@students.finki.ukim.mk>
2020-11-08 16:45:26 +0100

    render survey component with reactive forms

--- ng-reactive-survey/src/app/survey-renderer/components/survey-renderer/survey-renderer.component.ts
+++ ng-reactive-survey/src/app/survey-renderer/components/survey-renderer/survey-renderer.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Survey } from '../../../interfaces/survey.interface';
 import { QuestionType } from '../../../survey/enum/question-type.enum';
 import { timePickerDarkThemeConfig } from '../../config/time-picker-theme.config';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-survey-renderer',
@@ -10,17 +13,37 @@ import { timePickerDarkThemeConfig } from '../../config/time-picker-theme.config
 })
 export class SurveyRendererComponent implements OnInit {
 
-  @Input() surveyStructure: Survey;
+  @Input() surveyStructure$: Observable<Survey>;
+  surveyStructure: Survey;
 
   questionType = QuestionType;
   darkTheme = timePickerDarkThemeConfig;
+  form = this._builder.group({});
 
-  constructor() { }
+  constructor(
+    private _builder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.surveyStructure$.pipe(
+      tap(survey => this.initControls(survey))
+    ).subscribe(surveyStructure => this.surveyStructure = surveyStructure);
+  }
+
+  initControls(survey: Survey) {
+    survey?.questions?.forEach(q => {
+      if (q.questionType == QuestionType.CHECKBOX) {
+        const group = new FormGroup({});
+        q.options.forEach(o => group.addControl(o.id.toString(), new FormControl()));
+        this.form.addControl(q.id.toString(), group);
+      } else {
+        this.form.addControl(q.id.toString(), new FormControl());
+      }
+    });
   }
 
   submit() {
-
+    const value = this.form.getRawValue();
+    console.log(value);
   }
 }
