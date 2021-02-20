b5a15b83e25bbecdedd2125352074aae6e5a6bd0
Matej Bogdanoski <matej_bogdanoski@yahoo.com>
2021-02-20 22:57:23 +0100

    remove unneeded dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,31 +18,38 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.springframework.data:spring-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("io.r2dbc:r2dbc-postgresql")
+    implementation("org.flywaydb:flyway-core")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${coroutinesVersion}")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${coroutinesVersion}")
-    implementation("org.flywaydb:flyway-core")
-    implementation("io.r2dbc:r2dbc-spi")
-    implementation("io.r2dbc:r2dbc-postgresql")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+
     implementation("org.springframework.boot:spring-boot-starter-security")
+
     implementation("io.jsonwebtoken:jjwt:0.9.1")
+
     implementation("javax.xml.bind:jaxb-api:2.3.1")
 
-
     runtimeOnly("org.postgresql:postgresql")
+
+    implementation("io.projectreactor.tools:blockhound:1.0.4.RELEASE")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
     testImplementation("io.projectreactor:reactor-test")
-    implementation("io.projectreactor.tools:blockhound:1.0.4.RELEASE")
 }
 
 tasks.withType<Test> {
